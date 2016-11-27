07481ca4edf02d204e2fe17d40b2489477855d37
szymongor <gornioczek.szymon@gmail.com>
2016-11-27 12:50:11 +0100

    Api Client

---
+++ JSClasses/ApiClient.js
@@ -0,0 +1,92 @@
+function ApiClient(server){
+  this.server = server;
+
+  this.getPlayerId = function(){
+    var id;
+  	$.ajax({
+  		type: 'GET',
+      async: false,
+  		url: this.server+'/reg/api/playerId.php',
+  		success: function(data){
+  				id = ($.parseJSON(data))['id'];
+  		}
+  	});
+    return id;
+  };
+
+  this.getPlayerResources = function(method){
+    $.ajax({
+  		type: 'GET',
+  		url: this.server+'/reg/api/resources.php',
+  		success: function(data){
+  			resources = $.parseJSON(data);
+  			method(resources);
+  			}
+  		});
+  }
+
+  this.getTile = function(x,y,method){
+    $.ajax({
+  		type: 'GET',
+  		url: this.server+'/reg/api/map.php/tile/?x='+x+'&y='+y,
+  		success: function(data){
+  				method($.parseJSON(data));
+  		}
+  	});
+  }
+
+  this.getRegion = function(xFrom,xTo,yFrom,yTo,method){
+    $.ajax({
+  		type: 'GET',
+  		url: this.server+'/reg/api/map.php/region/?xFrom='+xFrom+'&xTo='+xTo+'&yFrom='+yFrom+'&yTo='+yTo,
+  		success: function(data){
+  				var region = $.parseJSON(data);
+  				$.each(region, function(i,row){
+  					$.each(row, function(j,value){
+  						method(value);
+  					});
+  				});
+  		}
+  	});
+  }
+
+  this.getBuildingDetails = function(x,y,method){
+    $.ajax({
+  		type: 'GET',
+  		url: this.server+'/reg/api/map.php/building/?x='+x+'&y='+y,
+  		success: function(data){
+  				method($.parseJSON(data));
+  		}
+  	});
+  }
+
+  this.getBuildingsToBuild = function(method){
+    $.ajax({
+  		type: 'GET',
+  		url: this.server+'/reg/api/building.php/buildingList',
+  		success: function(data){
+  				var buildingList = $.parseJSON(data);
+  				$.each(buildingList, function(i,value){
+  					method(value);
+  				});
+  		}
+  	});
+  }
+
+  this.conquer = function(x,y,method){
+    $.ajax({
+			type: 'GET',
+			url: this.server+'/reg/api/conquer.php?x='+x+'&y='+y,
+			success: function(data){
+				var newRegionTiles = $.parseJSON(data);
+				$.each(newRegionTiles, function(i,row){
+					$.each(row, function(j,value){
+						method(value);
+					});
+				});
+			}
+		});
+
+  }
+
+}
