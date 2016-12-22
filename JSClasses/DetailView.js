5ec311ca709885ef00154bb4cf15bfc1ef4d1b10
szymongor <gornioczek.szymon@gmail.com>
2016-12-22 13:06:11 +0100

    Showing buildings to build in details.

--- JSClasses/DetailView.js
+++ JSClasses/DetailView.js
@@ -1,5 +1,7 @@
 function DetailView(){
 
+  var DV= this;
+
   this.initDetailsView = function(){
     $('#gameDetails').empty();
     $('#gameDetails').append('<div id="gameDetailsOptionsContainer" class="gameDetailsOptionsContainer"></div>');
@@ -9,17 +11,15 @@ function DetailView(){
   }
 
   this.setDetailsMap = function(e){
-    $('#detailsView').empty();
-    $('#detailsView').append('Siemano');
-    console.log(e.detail);
+    //console.log(e.detail);
 
     var selectedTile = e.detail;
     if(selectedTile['building']==null){
   			$('#detailsView').empty();
   			$('#detailsView').append("No building here.")
   			var element = "tile" +selectedTile[0]+"x" +selectedTile[1];
-  			if($('#'+element).hasClass("ownedTile")){
-  				//showBuildingsToBuild();
+  			if(selectedTile.id_owner == idPlayer){
+  				DV.showBuildingsToBuild();
           console.log("showToBuild");
   			}
   		return;
@@ -27,6 +27,23 @@ function DetailView(){
   	var buildingStr = "Type: "+selectedTile.building.type;
   	$('#detailsView').empty();
   	$('#detailsView').append(buildingStr);
+
+  }
+
+  this.showBuildingsToBuild = function(){
+  	$('#detailsView').append("<div class='gameDetailsList' id='buildingsToBuildList'></div>");
+  	apiClient.getBuildingsToBuild(DV.appendBuildingToBuild);
+  }
+
+  this.appendBuildingToBuild = function(building){
+  	$('#buildingsToBuildList').append("<div class='gameDetailsBuildingToBuild' onclick=build('"+building.Type+"') id='"+building.Type+"ToBuild' ></div>");
+  	$('#'+building.Type+"ToBuild").append(building.Type + "<br/>");
+  	$('#'+building.Type+"ToBuild").append('<img src="img/Buildings/'+building.Type+'.png" height="70px" width="70px "/>');
+  	$('#'+building.Type+"ToBuild").append("<div class='gameDetailsBuildingToBuildResources' id='"+building.Type+"ToBuildCost' ></div>");
+  	$.each(building.Cost, function(i,value){
+  		if(value!=0)
+  		$('#'+building.Type+"ToBuildCost").append(i+":"+value+"<br/>");
+  	});
 
   }
 
