eafce96ca1f9a459f0162e7edfdb85e9854ce300
szymongor <gornioczek.szymon@gmail.com>
2017-02-25 16:36:07 +0100

    ArmyUnit functionality

--- engine/Army/ArmyUnit.php
+++ engine/Army/ArmyUnit.php
@@ -12,6 +12,7 @@
     public function __construct($type, $amount){
       $this->amount = $amount;
       $this->loadStatsInfo($type);
+      $this->calculateStats();
     }
 
     private function loadStatsInfo($type){
@@ -19,7 +20,15 @@
     }
 
     private function calculateStats(){
+      $a = $this->amount;
+      $stats = $this->statsInfo;
+      $this->health = $stats['Health'] * $a;
+      $this->defense = $stats['Defense'] * $a;
+      $this->attack = $stats['Atack'] * $a;
+    }
 
+    private function calculateAmount(){
+      $amount = floor($this->health / $this->statsInfo['Health']);
     }
 
     public function getStatsInfo(){
